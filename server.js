47e5ca6de5fcb6c6b7d20df5a009b42c0d4ea0e1
Oghomena <oghomenag@gmail.com>
2026-07-27 21:47:03 +0100

    cors fix

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const generatedUsers = require("./generated-users.json");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/generated-users", (req, res) => {
   res.json(generatedUsers);
